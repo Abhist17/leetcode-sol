8022895a52937900e6a8c79abde5766b75123f57
Abhist17 <abhistkamle.170903@gmail.com>
2026-02-20 19:13:58 +0530

    q761 special binary string

---
+++ Week1 (Feb-16)/q761.py
@@ -0,0 +1,29 @@
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        if len(s) <= 2:
+            
+            return s
+        
+        count = 0
+        start = 0
+        substrings = []
+        
+        
+        for i in range(len(s)):
+            if s[i] == '1':
+                count += 1
+            else:
+                count -= 1
+            
+           
+            if count == 0:
+                
+                middle = self.makeLargestSpecial(s[start+1:i])
+                substrings.append("1" + middle + "0")
+                start = i + 1
+        
+        
+        substrings.sort(reverse=True)
+        
+       
+        return "".join(substrings)
