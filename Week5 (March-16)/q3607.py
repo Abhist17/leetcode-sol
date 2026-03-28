9a2c321bca2d7d202612bd6a350e3fcc4629b328
Abhist17 <abhistkamle.170903@gmail.com>
2026-03-29 00:08:44 +0530

    more optimised sol

--- Week5 (March-16)/q3607.py
+++ Week5 (March-16)/q3607.py
@@ -1,10 +1,9 @@
 from typing import List
 from collections import defaultdict
-import bisect
+import heapq
 
 class Solution:
     def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
-      
         parent = list(range(c + 1))
         
         def find(x):
@@ -22,39 +21,27 @@ class Solution:
         
         comp = defaultdict(list)
         for i in range(1, c + 1):
-            root = find(i)
-            comp[root].append(i)
-       
+            comp[find(i)].append(i)
+        
+        comp_heap = {}
         for root in comp:
-            comp[root].sort()
+            heapq.heapify(comp[root])
+            comp_heap[root] = comp[root]
         
         active = [True] * (c + 1)
-    
-        comp_active = {}
-        for root in comp:
-            comp_active[root] = comp[root][:]  
-        
         ans = []
         
         for t, x in queries:
             root = find(x)
-            
             if t == 1:
                 if active[x]:
                     ans.append(x)
                 else:
-                    if comp_active[root]:
-                        ans.append(comp_active[root][0])
-                    else:
-                        ans.append(-1)
-            
-            else:  
-                if active[x]:
-                    active[x] = False
-                    
-                    lst = comp_active[root]
-                    idx = bisect.bisect_left(lst, x)
-                    if idx < len(lst) and lst[idx] == x:
-                        lst.pop(idx)
+                    heap = comp_heap[root]
+                    while heap and not active[heap[0]]:
+                        heapq.heappop(heap)
+                    ans.append(heap[0] if heap else -1)
+            else:
+                active[x] = False
         
         return ans
