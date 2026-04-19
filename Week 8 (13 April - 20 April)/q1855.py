76d77cef78e1405cdeb6e3cecfa4c57e6b9af58b
Abhist17 <abhistkamle.170903@gmail.com>
2026-04-19 15:13:05 +0530

    sol

---
+++ Week 8 (13 April - 20 April)/q1855.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        i = 0
+        j = 0
+        n1 = len(nums1)
+        n2 = len(nums2)
+        ans = 0
+        
+        while i < n1 and j < n2:
+            if nums1[i] <= nums2[j]:
+                ans = max(ans, j - i)
+                j += 1
+            else:
+                i += 1
+        
+        return ans
